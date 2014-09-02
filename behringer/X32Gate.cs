3b935ceceeec6d00543d97a23a4f52b96b0804ed
Daniel Goodnow <danielgoodnow@gmail.com>
2014-09-02 14:17:07 -0700

    move set s

--- behringer/X32Gate.cs
+++ behringer/X32Gate.cs
@@ -32,5 +32,49 @@ namespace gnow.util.behringer
          m_FilterType = Constants.FILTER_TYPE.LC6;
          m_FilterFrequency = 20.0f;
       }
+
+      public void setValuesFromOSC(string[] parameters, object value)
+      {
+          switch (parameters[2])
+          {
+              case "on":
+                  m_isOn = (Constants.ON_OFF)(int)value;
+                  break;
+              case "mode":
+                  m_Mode = (Constants.GATE_MODE)(int)value;
+                  break;
+              case "thr":
+                  m_Threshold.Value = (float)value;
+                  break;
+              case "range":
+                  m_Range.Value = (float)value;
+                  break;
+              case "attack":
+                  m_Attack.Value = (float)value;
+                  break;
+              case "hold":
+                  m_Hold = (float)value;
+                  break;
+              case "release":
+                  m_Release = (float)value;
+                  break;
+              case "keysrc":
+                  m_KeySource = (int)value;
+                  break;
+              default:
+                  switch (parameters[3])
+                  {
+                      case "on":
+                          m_FilterOn = (Constants.ON_OFF)(int)value;
+                          break;
+                      case "type":
+                          m_FilterType = (Constants.FILTER_TYPE)value;
+                          break;
+                      case "f":
+                          m_FilterFrequency = (float)value;
+                          break;
+                  }
+          }
+      }
    }
 }
